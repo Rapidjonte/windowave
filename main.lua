fff1f949a347e05bdccb28efdf7bf6d009d2e68e
rapid <61144105+Rapidjonte@users.noreply.github.com>
2025-06-15 23:14:48 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,60 @@
+require("bullet")
+
+width, height = 200, 200
+worldWidth, worldHeight = love.window.getDesktopDimensions(1)
+speed = 100
+playerSpeed = 100
+margin = 5
+
+sx, sy = 0, 0
+px, py = width / 2, height / 2
+ps = 10
+
+function love.load()
+   love.window.setMode(width, height, {borderless = true})
+   love.window.setPosition(sx, sy, 1)
+   love.graphics.setColor(1, 1, 1)
+end
+
+function love.update(dt)
+   local centerX = width / 2
+   local centerY = height / 2
+
+   if love.keyboard.isDown("escape") then
+      love.event.push('quit')
+   end
+
+    if love.keyboard.isDown("d") then
+      if math.abs(px - centerX-5) <= margin and sx + width < worldWidth then
+         sx = math.min(sx + speed * dt, worldWidth - width)
+      elseif px < width - 5 then
+         px = math.min(px + playerSpeed * dt, width - 5)
+      end
+   elseif love.keyboard.isDown("a") then
+      if math.abs(px - centerX) <= margin and sx > 0 then
+         sx = math.max(sx - speed * dt, 0)
+      elseif px > 5 then
+         px = math.max(px - playerSpeed * dt, 5)
+      end
+   end
+
+   if love.keyboard.isDown("s") then
+      if math.abs(py - centerY-5) <= margin and sy + height < worldHeight then
+         sy = math.min(sy + speed * dt, worldHeight - height)
+      elseif py < height - 5 then
+         py = math.min(py + playerSpeed * dt, height - 5)
+      end
+   elseif love.keyboard.isDown("w") then
+      if math.abs(py - centerY) <= margin and sy > 0 then
+         sy = math.max(sy - speed * dt, 0)
+      elseif py > 5 then
+         py = math.max(py - playerSpeed * dt, 5)
+      end
+   end
+end
+
+function love.draw()
+   love.graphics.clear()
+   love.graphics.rectangle("fill", px - 5, py - 5, ps, ps)
+   love.window.setPosition(sx, sy, 1)
+end
